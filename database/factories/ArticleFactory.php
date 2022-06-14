922d8b79d72320b8fd987a38b5d9015f14e7e6ad
Joy Das <joydascsepuc@gmail.com>
2022-06-14 23:09:36 +0600

    starting point of laravel-vue

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    protected $model = Article::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(50),
+            'body'  => $this->faker->text(200)
+        ];
+    }
+}
